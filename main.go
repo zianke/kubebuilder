f253a87cf9bece38f496cd387bb50f4d7179a168
Phillip Wittrock <pwittroc@google.com>
2018-06-05 13:49:49 -0700

    Fix main

--- main.go
+++ main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-	"os"
 	"flag"
+	"os"
 
-	logf "github.com/kubernetes-sigs/kubebuilder/pkg/log"
-	kcache "k8s.io/client-go/tools/cache" // $cashmoney
-	corev1 "k8s.io/api/core/v1"
 	"github.com/kubernetes-sigs/kubebuilder/pkg/informer"
+	logf "github.com/kubernetes-sigs/kubebuilder/pkg/log"
+	corev1 "k8s.io/api/core/v1"
+	kcache "k8s.io/client-go/tools/cache" // $cashmoney
 )
 
 func main() {
 	flag.Parse()
 
 	/*
-	cfg, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
-	if err != nil {
-		log.Error(err, "could not initialize kubernetes client")
-		os.Exit(1)
-	}
-	
-	// NB: this should really only be done once for improved startup time
-	
-	discoClient := discovery.NewDiscoveryClientForConfigOrDie(cfg)
-	groupReses, err := discovery.GetAPIGroupResources(discoClient)
-	if err != nil {
-		log.Error(err, "could not fetch API discovery information") 
-		os.Exit(1)
-	}
-	discoMapper := discovery.NewRESTMapper(groupReses, dynamic.VersionInterfaces)*/
+		cfg, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
+		if err != nil {
+			log.Error(err, "could not initialize kubernetes client")
+			os.Exit(1)
+		}
+
+		// NB: this should really only be done once for improved startup time
+
+		discoClient := discovery.NewDiscoveryClientForConfigOrDie(cfg)
+		groupReses, err := discovery.GetAPIGroupResources(discoClient)
+		if err != nil {
+			log.Error(err, "could not fetch API discovery information")
+			os.Exit(1)
+		}
+		discoMapper := discovery.NewRESTMapper(groupReses, dynamic.VersionInterfaces)*/
 
 	log := logf.Log.WithName("main")
 	logf.SetLogger(logf.ZapLogger(true))
 	log.Info("initializing...")
 
-	cache := &informer.IndexedCache{}
+	cache := &informer.SelfPopulatingInformers{}
 	podInformer, err := cache.InformerFor(&corev1.Pod{})
 	if err != nil {
 		log.Error(err, "could not initialize informer", "kind", "Pod")
@@ -49,5 +49,5 @@ func main() {
 		},
 	})
 
-	select{}
+	select {}
 }
