ba7419d01bcd618cbdd5c0f208e90275b996425a
Phillip Wittrock <pwittroc@google.com>
2018-06-06 22:06:54 -0700

    Fix Main

--- main.go
+++ main.go
@@ -23,44 +23,41 @@ func main() {
 	flag.Parse()
 	logf.SetLogger(logf.ZapLogger(true))
 
-	// Init Controller
-	c := &ctrl.Controller{
-		Reconcile: reconcile.ReconcileFunc(func(request reconcile.ReconcileRequest) (reconcile.ReconcileResult, error) {
-			log.Info("Got Reconcile", "Request", request)
-			return reconcile.ReconcileResult{}, nil
-		}),
-	}
-	cfg, err := config.GetConfig()
-	if err != nil || cfg == nil {
+	// Init controller
+	cm, err := ctrl.NewControllerManager(ctrl.ControllerManagerArgs{Config: config.GetConfigOrDie()})
+	if err != nil {
 		log.Error(err, "Could not get a rest.Config")
+		os.Exit(1)
 	}
-	cm := &ctrl.ControllerManager{
-		Config: cfg,
-	}
-	cm.AddController(c, func() {
-		c.Watch(&source.KindSource{Type: &corev1.Endpoints{}}, &eventhandler.EnqueueHandler{})
-		c.FieldIndexes.IndexField(&corev1.Endpoints{}, "synthetic.targets", func(obj runtime.Object) []string {
-			ep := obj.(*corev1.Endpoints)
-			var res []string
-			for _, subset := range ep.Subsets {
-				for _, addr := range subset.Addresses {
-					res = append(res, addr.TargetRef.Name)
-				}
+	cm.GetFieldIndexer().IndexField(&corev1.Endpoints{}, "synthetic.targets", func(obj runtime.Object) []string {
+		ep := obj.(*corev1.Endpoints)
+		var res []string
+		for _, subset := range ep.Subsets {
+			for _, addr := range subset.Addresses {
+				res = append(res, addr.TargetRef.Name)
 			}
-			return res
-		})
-		c.Watch(&source.KindSource{Type: &corev1.Pod{}}, &eventhandler.EnqueueMappedHandler{
-			ToRequests: eventhandler.ToRequestsFunc(func(evt eventhandler.MapObject) []reconcile.ReconcileRequest {
-				pods := &corev1.PodList{}
-				c.Client.List(context.TODO(), client.MatchingField("synthetic.targets", evt.Meta.GetName()), pods)
-				return nil
-			}),
-		})
+		}
+		return res
+	})
+
+	c := cm.NewController(ctrl.ControllerArgs{Name: "sample-controller"},
+		reconcile.ReconcileFunc(func(request reconcile.ReconcileRequest) (reconcile.ReconcileResult, error) {
+			log.Info("Got reconcile", "Request", request)
+			return reconcile.ReconcileResult{}, nil
+		}))
+
+	c.Watch(&source.KindSource{Type: &corev1.Endpoints{}}, &eventhandler.EnqueueHandler{})
+	c.Watch(&source.KindSource{Type: &corev1.Pod{}}, &eventhandler.EnqueueMappedHandler{
+		ToRequests: eventhandler.ToRequestsFunc(func(evt eventhandler.MapObject) []reconcile.ReconcileRequest {
+			pods := &corev1.PodList{}
+			cm.GetClient().List(context.TODO(), client.MatchingField("synthetic.targets", evt.Meta.GetName()), pods)
+			return nil
+		}),
 	})
 
 	// Start main
 	if err := cm.Start(make(chan struct{})); err != nil {
-		log.Error(err, "Failed to Start ControllerManager")
+		log.Error(err, "Failed to Start controllerManager")
 		os.Exit(1)
 	}
 }
